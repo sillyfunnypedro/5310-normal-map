fc7649b20a01985e198bd004e9e2e5f8098e2d5d
sillyfunnypedro <sillyfunnypedro@gmail.com>
2023-08-25 06:51:43 +0200

    more cleanup

--- src/README.md
+++
@@ -1,6 +0,0 @@
-# SRC
-This is where your files go.   Any files you add should be here.
-
-Please do not add binaries here.
-
-
--- src/shaders/FragmentShader.ts
+++
@@ -1,9 +0,0 @@
-export const fragmentShader =
-    `#version 300 es
-    precision mediump float;
-    in vec3 vColor;
-    out vec4 color;
-    void main() {
-        color = vec4(vColor, 1);
-    }
-    `;
