b5c20cde2b34cac243c416275749d9cd10cd7ddb
sillyfunnypedro <sillyfunnypedro@gmail.com>
2023-09-02 12:32:48 -0700

    texture seems to be workign

--- part1/src/ObjectFileMap.ts
+++ part1/src/ObjectFileMap.ts
@@ -1,8 +1,9 @@
 export const objectFileMap: Map<string, string> = new Map([
     ["tri-plain", "tri_plain/triangle.obj"],
     ["tri-tex", "triangle_texture/triangle.obj"],
-    // ["square", "square/square.obj"],
-    // ["cube", "cube/cube.obj"],
+    ["square", "square/square.obj"],
+    ["cube", "cube/cube.obj"],
+    ["cube-tex", "textured_cube/cube.obj"],
     // ["capsule", "capsule/capsule.obj"],
     // ["chapel", "chapel/chapel_obj.obj"],
     // ["bunny_centered", "bunny/bunny_centered.obj"],
