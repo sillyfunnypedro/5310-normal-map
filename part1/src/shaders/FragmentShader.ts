1b3b73d8383230d76d9f909d6b1db923ee958f21
sillyfunnypedro <sillyfunnypedro@gmail.com>
2023-10-18 12:22:24 -0700

    lambert ligthing in shader

--- part1/src/shaders/FragmentShader.ts
+++ part1/src/shaders/FragmentShader.ts
@@ -48,14 +48,31 @@ const fragmentTextureNormalShader =
 
     void main() {
         vec3 normal = normalize(normalOut);
-        vec3 lightDirection = normalize(vec3(0,1, 0));
+        vec3 lightDirection = normalize(vec3(1,1, 1));
+        vec4 lightColor = vec4(1.0, 1.0, 1.0, 1.0);
+        float ambientIntensity = 0.0;
         float lightIntensity = dot(normal, lightDirection);
+ 
+
+        // clamp the light intensity to between 0 and 1
+        lightIntensity = clamp(lightIntensity, 0.0, 1.0);
+
+        // clamp the light intensity to between 0 and 1
+
+        
+        // scale the light color to the light intensity
+        lightColor = lightColor * lightIntensity;
+
+
         lightIntensity = clamp(lightIntensity, 0.0, 1.0);
         vec2 textureCoord = vec2(textureCoordOut.x, 1.0 - textureCoordOut.y);
         vec4 textureColor = texture(textureSampler, textureCoord);
-        
-        color = normalize(vec4(textureColor.rgb * lightIntensity, textureColor.a));
-       
+
+        // multiply the color by the light intensity (after you get the texture value)
+
+        color = vec4(textureColor.rgb*lightColor.rgb, 1.0);
+
+
     }
     `;
 
